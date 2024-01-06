3b34c6cc444d01dfdea8d83ff385eeafec16f50e
aloha-fim <frederec.im@gmail.com>
2024-01-06 13:44:32 +0800

    TDD headers

--- auto_gippity/src/apis/call_request.rs
+++ auto_gippity/src/apis/call_request.rs
@@ -10,7 +10,7 @@ pub async fn call_gpt(messages: Vec<Message>) {
     dotenv().ok();
 
     // Extract API Key Information
-    let api_key: String = env::var("OPEN_API_KEY").expect("OPEN_API_KEY not found in environment variables");
+    let api_key: String = env::var("OPEN_AI_KEY").expect("OPEN_API_KEY not found in environment variables");
     let api_org: String = env::var("OPEN_AI_ORG").expect("OPEN_API_ORG not found in environment variables");
 
     //Confirm endpoint
@@ -53,4 +53,24 @@ pub async fn call_gpt(messages: Vec<Message>) {
         .unwrap();
 
     dbg!(res_raw.text().await.unwrap());
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn tests_call_to_openai() {
+
+        let message: Message = Message {
+            role: "user".to_string(),
+            content: "Hi there, this is a test. Give me a short response.".to_string()
+        };
+
+        let messages: Vec<Message> = vec!(message);
+
+        call_gpt(messages).await;
+    }
+
 }
