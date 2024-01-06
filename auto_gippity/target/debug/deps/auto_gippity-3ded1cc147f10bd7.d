c4040db9a179ef9398fae62b5046b6c205097a11
aloha-fim <frederec.im@gmail.com>
2024-01-06 15:45:06 +0800

    import ai functions

--- auto_gippity/target/debug/deps/auto_gippity-3ded1cc147f10bd7.d
+++ auto_gippity/target/debug/deps/auto_gippity-3ded1cc147f10bd7.d
@@ -1,9 +1,12 @@
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-3ded1cc147f10bd7.rmeta: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-3ded1cc147f10bd7.rmeta: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
 
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-3ded1cc147f10bd7.d: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-3ded1cc147f10bd7.d: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
 
 src\main.rs:
 src\ai_functions\mod.rs:
+src\ai_functions\aifunc_architect.rs:
+src\ai_functions\aifunc_backend.rs:
+src\ai_functions\aifunc_managing.rs:
 src\apis\mod.rs:
 src\apis\call_request.rs:
 src\helpers\mod.rs:
--- auto_gippity/target/debug/deps/auto_gippity-43166611e39ebf1d.d
+++ auto_gippity/target/debug/deps/auto_gippity-43166611e39ebf1d.d
@@ -1,9 +1,12 @@
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-43166611e39ebf1d.rmeta: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-43166611e39ebf1d.rmeta: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
 
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-43166611e39ebf1d.d: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-43166611e39ebf1d.d: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
 
 src\main.rs:
 src\ai_functions\mod.rs:
+src\ai_functions\aifunc_architect.rs:
+src\ai_functions\aifunc_backend.rs:
+src\ai_functions\aifunc_managing.rs:
 src\apis\mod.rs:
 src\apis\call_request.rs:
 src\helpers\mod.rs:
