44028e5aae18b13a1a2106765d12bf646a9970ba
aloha-fim <frederec.im@gmail.com>
2024-01-06 13:11:14 +0800

    url for gpt call endpoint

---
+++ auto_gippity/src/apis/call_request.rs
@@ -0,0 +1,20 @@
+use crate::models::general::llm::{ Message };
+use dotenv::dotenv;
+use reqwest::Client;
+use std::env;
+
+// Call Large Language Model
+pub async fn call_gpt(messages: Vec<Message>) {
+    dotenv().ok();
+
+    // Extract API Key Information
+    let api_key: String = env::var("OPEN_API_KEY").expect("OPEN_API_KEY not found in environment variables");
+    let api_org: String = env::var("OPEN_AI_ORG").expect("OPEN_API_ORG not found in environment variables");
+
+    //Confirm endpoint
+    let url: &str = "https://api.openai.com/v1/chat/completions";
+
+    //Create api key header
+
+
+}
--- auto_gippity/target/debug/deps/auto_gippity-3ded1cc147f10bd7.d
+++ auto_gippity/target/debug/deps/auto_gippity-3ded1cc147f10bd7.d
@@ -1,10 +1,13 @@
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-3ded1cc147f10bd7.rmeta: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-3ded1cc147f10bd7.rmeta: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
 
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-3ded1cc147f10bd7.d: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-3ded1cc147f10bd7.d: src\main.rs src\ai_functions\mod.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
 
 src\main.rs:
 src\ai_functions\mod.rs:
 src\apis\mod.rs:
+src\apis\call_request.rs:
 src\helpers\mod.rs:
 src\helpers\command_line.rs:
 src\models\mod.rs:
+src\models\general\mod.rs:
+src\models\general\llm.rs:
