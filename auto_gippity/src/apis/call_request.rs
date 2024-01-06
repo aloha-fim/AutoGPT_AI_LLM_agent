96c843e0786cdb357023ca25acb66a30fbb9d8bf
aloha-fim <frederec.im@gmail.com>
2024-01-06 15:34:52 +0800

    api call with async fn with error control and response

--- auto_gippity/src/apis/call_request.rs
+++ auto_gippity/src/apis/call_request.rs
@@ -1,4 +1,4 @@
-use crate::models::general::llm::{ Message, ChatCompletion };
+use crate::models::general::llm::{ Message, ChatCompletion, APIResponse };
 use dotenv::dotenv;
 use reqwest::Client;
 use std::env;
@@ -6,7 +6,7 @@ use std::env;
 use reqwest::header::{ HeaderMap, HeaderValue };
 
 // Call Large Language Model
-pub async fn call_gpt(messages: Vec<Message>) {
+pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error + Send>> {
     dotenv().ok();
 
     // Extract API Key Information
@@ -22,20 +22,22 @@ pub async fn call_gpt(messages: Vec<Message>) {
     //Create api key header
     headers.insert(
         "authorization",
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap()
+        HeaderValue::from_str(&format!("Bearer {}", api_key))
+        .map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?
     );
 
     //Create Open AI Org header
     headers.insert(
         "OpenAI-Organization",
-        HeaderValue::from_str(api_org.as_str()).unwrap()
+        HeaderValue::from_str(api_org.as_str())
+        .map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?
     );
 
     //Create client with low temp (less creative parameter)
     let client: Client = Client::builder()
         .default_headers(headers)
         .build()
-        .unwrap();
+        .map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?;
 
     //Create chat completion
     let chat_completion: ChatCompletion = ChatCompletion {
@@ -45,14 +47,27 @@ pub async fn call_gpt(messages: Vec<Message>) {
     };
 
     // troubleshooting in case API fails
-    let res_raw = client
+    // let res_raw = client
+    //     .post(url)
+    //     .json(&chat_completion)
+    //     .send()
+    //     .await
+    //     .unwrap();
+    // dbg!(res_raw.text().await.unwrap());
+
+    //Extract API Response
+    let res: APIResponse = client
         .post(url)
         .json(&chat_completion)
         .send()
         .await
-        .unwrap();
+        .map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?
+        .json()
+        .await
+        .map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?;
 
-    dbg!(res_raw.text().await.unwrap());
+ // Send Response
+ Ok(res.choices[0].message.content.clone())
 }
 
 
@@ -70,7 +85,15 @@ mod tests {
 
         let messages: Vec<Message> = vec!(message);
 
-        call_gpt(messages).await;
+        let res: Result<String, Box<dyn std::error::Error + Send>> = call_gpt(messages).await;
+        match res {
+            Ok(res_str) => {
+                dbg!(res_str);
+                assert!(true);
+            },
+            Err(_) => {
+                assert!(false);
+            }
+        }
     }
-
 }
