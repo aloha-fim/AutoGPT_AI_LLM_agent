427689056a69b1456b673f56692033f420232a9f
aloha-fim <frederec.im@gmail.com>
2024-01-09 13:38:01 +0800

    create managing agent factsheet

--- auto_gippity/target/debug/deps/auto_gippity-43166611e39ebf1d.d
+++ auto_gippity/target/debug/deps/auto_gippity-43166611e39ebf1d.d
@@ -1,6 +1,6 @@
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-43166611e39ebf1d.rmeta: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\helpers\general.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\libauto_gippity-43166611e39ebf1d.rmeta: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\helpers\general.rs src\models\mod.rs src\models\agent_basic\mod.rs src\models\agent_basic\basic_agent.rs src\models\agent_basic\basic_traits.rs src\models\agents\mod.rs src\models\agents\agent_architect.rs src\models\agents\agent_backend.rs src\models\agents\agent_traits.rs src\models\general\mod.rs src\models\general\llm.rs
 
-C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-43166611e39ebf1d.d: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\helpers\general.rs src\models\mod.rs src\models\general\mod.rs src\models\general\llm.rs
+C:\Users\frede\harvard\s14a\AutoGPT_AI_LLM_agent\auto_gippity\target\debug\deps\auto_gippity-43166611e39ebf1d.d: src\main.rs src\ai_functions\mod.rs src\ai_functions\aifunc_architect.rs src\ai_functions\aifunc_backend.rs src\ai_functions\aifunc_managing.rs src\apis\mod.rs src\apis\call_request.rs src\helpers\mod.rs src\helpers\command_line.rs src\helpers\general.rs src\models\mod.rs src\models\agent_basic\mod.rs src\models\agent_basic\basic_agent.rs src\models\agent_basic\basic_traits.rs src\models\agents\mod.rs src\models\agents\agent_architect.rs src\models\agents\agent_backend.rs src\models\agents\agent_traits.rs src\models\general\mod.rs src\models\general\llm.rs
 
 src\main.rs:
 src\ai_functions\mod.rs:
@@ -13,5 +13,12 @@ src\helpers\mod.rs:
 src\helpers\command_line.rs:
 src\helpers\general.rs:
 src\models\mod.rs:
+src\models\agent_basic\mod.rs:
+src\models\agent_basic\basic_agent.rs:
+src\models\agent_basic\basic_traits.rs:
+src\models\agents\mod.rs:
+src\models\agents\agent_architect.rs:
+src\models\agents\agent_backend.rs:
+src\models\agents\agent_traits.rs:
 src\models\general\mod.rs:
 src\models\general\llm.rs:
