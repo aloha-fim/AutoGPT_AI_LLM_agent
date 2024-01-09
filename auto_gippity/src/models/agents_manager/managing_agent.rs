e012923cfa7d243fb34a916429b17ee804ec5240
aloha-fim <frederec.im@gmail.com>
2024-01-09 18:30:34 +0800

    write test for managing agent to pass with execution function

--- auto_gippity/src/models/agents_manager/managing_agent.rs
+++ auto_gippity/src/models/agents_manager/managing_agent.rs
@@ -47,4 +47,48 @@ impl ManagingAgent {
             agents
         })
     }
+
+    fn add_agent(&mut self, agent: Box<dyn SpecialFunctions>) {
+        self.agents.push(agent);
+    }
+
+    fn create_agents(&mut self) {
+        self.add_agent(Box::new(AgentSolutionArchitect::new()));
+    }
+
+    pub async fn execute_project(&mut self) {
+        self.create_agents();
+
+        for agent in &mut self.agents {
+
+            let agent_res: Result<(), Box<dyn std::error::Error>> =
+            agent.execute(&mut self.factsheet).await;
+
+            let agent_info: &BasicAgent = agent.get_attributes_from_agent();
+            dbg!(agent_info);
+        }
+    }
+}
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use crossterm::style::Stylize;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn tests_managing_agent() {
+        let usr_request: &str = "need a full stack app that fetches my fitness progress. Needs to include timezone info from the web.";
+
+        let mut managing_agent: ManagingAgent = ManagingAgent::new(usr_request.to_string()).await.expect("Error creating Managing Agent");
+
+        managing_agent.execute_project().await;
+
+        dbg!(managing_agent.factsheet);
+    }
 }
